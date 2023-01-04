2e8db243cc92fe28a4622d5e4b5c5fcc2143bb4d
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2023-01-04 10:19:29 +0530

    Initial Commit

---
+++ Notes WatchKit Extension/NotesApp.swift
@@ -0,0 +1,19 @@
+//
+//  NotesApp.swift
+//  Notes WatchKit Extension
+//
+//  Created by Ashish Sharma on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
